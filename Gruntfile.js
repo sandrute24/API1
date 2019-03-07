9d287e5ad8f942f5eb455738a75c8156df2c0a8c
Sandra Saukaite <sandra.saukaite@shopify.com>
2019-03-07 21:26:08 +0200

    changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,107 +2,103 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-      compass: {
-          options: {
-              sassDir: 'src/assets/sass',
-              imagesDir: 'src/img',
-              cssDir: 'web/assets/css',
-              force: true
-          },
-          dist: {
-              options: {
-                  environment: 'development',
-                  noLineComments: true
-              }
-          },
-          dev: {
-              options: {
-                  noLineComments: true
-              }
-          },
-          watch: {
-              options: {
-                  noLineComments: true,
-                  watch: true
-              }
-          }
+    compass: {
+      options: {
+        sassDir: 'src/assets/sass',
+        imagesDir: 'src/img',
+        cssDir: 'web/assets/css',
+        force: true
       },
-      assemble: {
+      dist: {
         options: {
-            layoutdir: 'src/templates/layouts',
-            layout: ['default.hbs'],
-            partials: ['src/templates/partials/{,*/}*.*', 'src/sprites/svg/*'],
-            helpers: ['partial'],
-            flatten: true
+          environment: 'development',
+          noLineComments: true
+        }
+      },
+      dev: {
+        options: {
+          noLineComments: true
+        }
+      },
+      watch: {
+        options: {
+          noLineComments: true,
+          watch: true
+        }
+      }
+    },
+    assemble: {
+      options: {
+        layoutdir: 'src/templates/layouts',
+        layout: ['default.hbs'],
+        partials: ['src/templates/partials/{,*/}*.*', 'src/sprites/svg/*'],
+        helpers: ['partial'],
+        flatten: true
+      },
+      en: {
+        options: {
+          data: ['src/templates/data/en/*.yml']
         },
-        en: {
-            options: {
-                data: ['src/templates/data/en/*.yml', 'src/templates/data/*.yml']
-            },
-            src: ['src/templates/pages/en/*.hbs'],
-            dest: './web'
+        src: ['src/templates/pages/en/*.hbs'],
+        dest: './web'
+      },
+      lt: {
+        options: {
+          data: ['src/templates/data/lt/*.yml']
         },
+        src: ['src/templates/pages/lt/*.hbs'],
+        dest: './web/lt/'
+      },
     },
     watch: {
-        options: {
-        },
-        dev: {
-            files: ['src/assets/sass/**/*.scss', 'src/templates/**/*.hbs'],
-            tasks: ['compass:dev', 'assemble:en']
-        },
-        handlebars: {
-            files: ['src/templates/*/*.hbs', 'src/templates/layouts/*.hbs' ],
-            tasks: ['assemble:en']
-        }
+      options: {},
+      dev: {
+        files: ['src/assets/sass/**/*.scss', 'src/templates/**/*.hbs'],
+        tasks: ['compass:dev', 'assemble:site']
+      },
+      handlebars: {
+        files: ['src/templates/*/*.hbs', 'src/templates/layouts/*.hbs'],
+        tasks: ['assemble:site']
+      }
     },
 
     svg_sprite: {
-            generate: {
-                cwd: 'web/assets/vendor/material-design-icons',
-                src: [
-                    '../../../../web/assets/images/ic_menu_24px.svg',
-                    '../../../../web/assets/images/ic_notification_24px.svg',
-                    '../../../../web/assets/images/ic_home_24px.svg',
-                    '../../../../web/assets/images/ic_build_24px.svg',
-                    '../../../../web/assets/images/ic_indicator_24px.svg',
-                    '../../../../web/assets/images/ic_love_24px.svg',
-                    '../../../../web/assets/images/ic_offline_24px.svg',
-                    '../../../../web/assets/images/ic_pets_24px.svg',
-                    '../../../../web/assets/images/ic_star_24px.svg',
-                    '../../../../web/assets/images/ic_dashboard_24px.svg',
-                    '../../../../web/assets/images/ic_language_24px.svg',
-                    '../../../../web/assets/images/ic_loyalty_24px.svg',
-                    '../../../../web/assets/images/ic_front_24px.svg',
+      generate: {
+        cwd: 'web/assets/vendor/material-design-icons',
+        src: [
+          '../../../../web/assets/images/ic_menu_24px.svg',
+          '../../../../web/assets/images/ic_notification_24px.svg',
+          '../../../../web/assets/images/ic_home_24px.svg',
+          '../../../../web/assets/images/ic_build_24px.svg',
+          '../../../../web/assets/images/ic_indicator_24px.svg',
+          '../../../../web/assets/images/ic_love_24px.svg',
+          '../../../../web/assets/images/ic_offline_24px.svg',
+          '../../../../web/assets/images/ic_pets_24px.svg',
+          '../../../../web/assets/images/ic_star_24px.svg',
+          '../../../../web/assets/images/ic_dashboard_24px.svg',
+          '../../../../web/assets/images/ic_language_24px.svg',
+          '../../../../web/assets/images/ic_loyalty_24px.svg',
+          '../../../../web/assets/images/ic_front_24px.svg',
 
-
-
-
-
-
-
-
-
-
-
-                ],
-                dest: 'src/sprites',
-                options: {
-                    shape: {
-                        id: {
-                            generator: function(filename) {
-                                var id = filename.match(/ic_(\w+)_\d+/);
-                                return id[1];
-                            }
-                        },
-                    },
-                    mode: {
-                        symbol: {
-                            dest: ''
-                        }
-                    }
-                }
+        ],
+        dest: 'src/sprites',
+        options: {
+          shape: {
+            id: {
+              generator: function(filename) {
+                var id = filename.match(/ic_(\w+)_\d+/);
+                return id[1];
+              }
+            },
+          },
+          mode: {
+            symbol: {
+              dest: ''
             }
-        },
+          }
+        }
+      }
+    },
   });
 
   [
@@ -110,12 +106,17 @@ module.exports = function(grunt) {
     'grunt-contrib-watch',
     'grunt-assemble',
     'grunt-svg-sprite'
-].forEach(grunt.loadNpmTasks);
+  ].forEach(grunt.loadNpmTasks);
 
-  // Default task(s).
+  grunt.registerTask('assemble:site', [
+    'assemble:en',
+    'assemble:lt',
+  ]);
+
   grunt.registerTask('default', [
-      'compass:dist',
-      'assemble:en'
+    'compass:dist',
+    'assemble:en',
+    'assemble:lt',
   ]);
 
 };
