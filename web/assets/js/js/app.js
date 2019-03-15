518bf911f029ed93d5519c495f86833158816691
Sandra Saukaite <sandra.saukaite@shopify.com>
2019-03-15 08:39:03 +0200

    march14

--- web/assets/js/js/app.js
+++ web/assets/js/js/app.js
@@ -1,5 +1,10 @@
 $(function() {
   $('#burger').on('click', function() {
     $('#side-menu').toggleClass('side-menu--closed');
+    $('body').toggleClass('sidenav-closed');
   })
-})
+
+var basename = $('#basename') .text($'#side-menu') .find('li.active span').text());
+$('#basename').text(basename)
+.attr('href', `/${basename.toLowerCase()}.html`);
+ })
--- web/assets/js/js/chart.js
+++ web/assets/js/js/chart.js
@@ -1,65 +1,76 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    type: 'line',
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
     data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [{
-            label: "My First dataset",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
-        }]
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
     },
-
-    // Configuration options go here
-    options: {}
-});
-
-
-
-
-
-
-var ctx = document.getElementById('myChart2').getContext('2d');
-var chart = new Chart(ctx, {
-// The type of chart we want to create
-type: 'pie',
-
-// The data for our dataset
-data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-        label: "My First dataset",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-},
-
-// Configuration options go here
-options: {}
-});
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 
 
 
+  var ctxP = document.getElementById("myChart2").getContext('2d');
+    var myPieChart = new Chart(ctxP, {
+      type: 'pie',
+      data: {
+        labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+        datasets: [{
+          data: [300, 50, 100, 40, 120],
+          backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+          hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+        }]
+      },
+      options: {
+        responsive: true
+      }
+    });
 
 
-var ctx = document.getElementById('myChart3').getContext('2d');
-var chart = new Chart(ctx, {
-// The type of chart we want to create
-type: 'radar',
-
-// The data for our dataset
-data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-        label: "My First dataset",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-},
-
-// Configuration options go here
-options: {}
-});
+    var ctxPA = document.getElementById("myChart3").getContext('2d');
+      var myPolarChart = new Chart(ctxPA, {
+        type: 'polarArea',
+        data: {
+          labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+          datasets: [{
+            data: [300, 50, 100, 40, 120],
+            backgroundColor: ["rgba(255,99,132,1)", "rgba(54, 162, 235, 1)", "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)", "rgba(0, 0, 0, 0.3)"
+            ],
+            hoverBackgroundColor: ["rgba(219, 0, 0, 0.2)", "rgba(0, 165, 2, 0.2)",
+              "rgba(255, 195, 15, 0.3)", "rgba(55, 59, 66, 0.1)", "rgba(0, 0, 0, 0.4)"
+            ]
+          }]
+        },
+        options: {
+          responsive: true
+        }
+      });
