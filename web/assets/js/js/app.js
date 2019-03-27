fd86b0f483a3ddb9fdf68a90dbb3a000622ac9e3
Sandra Saukaite <sandra.saukaite@shopify.com>
2019-03-27 21:11:25 +0200

    added sidemenu-right, edited profiel.hbs

--- web/assets/js/js/app.js
+++ web/assets/js/js/app.js
@@ -1,8 +1,19 @@
 $(function() {
+
+
   $('#burger').on('click', function() {
     $('#side-menu').toggleClass('side-menu--closed');
     $('body').toggleClass('sidenav-closed');
-  })
+  });
+
+
+  $('#notification').on('click', function() {
+    $('#side-menu-right').toggleClass('side-menu--right--closed');
+  });
+
+
+
+
 
 var basename = $('#basename') .text('#side-menu') .find('li.active span').text();
 $('#basename').text(basename)
